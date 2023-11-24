1ffd2fe95bff82e10fec5cd3bcc3415751cb2d20
Toxic-Whitlgnd <whitelegend56@gmail.com>
2023-11-24 23:51:13 +0530

    added requirement files

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from pymongo import MongoClient
 from geopy.geocoders import Nominatim
-import requests
 import bcrypt,random
 import pickle
 import xgboost as xgb
 import numpy as np
 
-from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
